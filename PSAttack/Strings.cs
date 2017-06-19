d742507dc258fb37f07210db9bc3a2bf0c0a1523
Jared Haight <jared@photosandtext.com>
2017-06-18 18:30:19 -0700

    version bump

--- PSAttack/Strings.cs
+++ PSAttack/Strings.cs
@@ -7,7 +7,7 @@ namespace PSAttack
 {
     class Strings
     {
-        public static string version = "1.8.1";
+        public static string version = "1.9";
         public static string windowTitle = "PSAttack!!";
         public static List<string> psaLogos = new List<string>() {
 @"
